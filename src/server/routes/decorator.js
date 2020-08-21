896a92b798dbc3431388553a3d09b9396e132ac5
Sindre Dahl Løken <sindre.loken@nav.no>
2020-08-21 14:43:00 +0200

    Oppdatert dekorator urler og lagt til tags i index.html Co-authored-by: Mats Bolstad <mats.bolstad@nav.no> Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>

--- src/server/routes/decorator.js
+++ src/server/routes/decorator.js
@@ -1,40 +1,42 @@
-const jsdom = require('jsdom');
-const request = require('request');
+const jsdom = require("jsdom");
+const request = require("request");
 
 const { JSDOM } = jsdom;
-url = '';
-if (process.env.NAIS_CLUSTER_NAME === 'prod-sbs') {
-    url =
-        'https://appres.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true&skiplinks=true&megamenu-resources=true';
+let url = "";
+if (process.env.NAIS_CLUSTER_NAME === "prod-gcp") {
+  url =
+    "https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&level=Level4/no/";
 } else {
-    url =
-        'https://appres-q1.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true&skiplinks=true&megamenu-resources=true';
+  url =
+    "https://www.nav-q0.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&level=Level4/no/";
 }
 
 const requestDecorator = callback => request(url, callback);
 
 const getDecorator = () =>
-    new Promise((resolve, reject) => {
-        const callback = (error, response, body) => {
-            if (!error && response.statusCode >= 200 && response.statusCode < 400) {
-                const { document } = new JSDOM(body).window;
-                const prop = 'innerHTML';
+  new Promise((resolve, reject) => {
+    const callback = (error, response, body) => {
+      if (!error && response.statusCode >= 200 && response.statusCode < 400) {
+        const { document } = new JSDOM(body).window;
+        const prop = "innerHTML";
 
-                const data = {
-                    NAV_SCRIPTS: document.getElementById('scripts')[prop],
-                    NAV_STYLES: document.getElementById('styles')[prop],
-                    NAV_SKIPLINKS: document.getElementById('skiplinks')[prop],
-                    NAV_HEADING: document.getElementById('header-withmenu')[prop],
-                    NAV_FOOTER: document.getElementById('footer-withmenu')[prop],
-                    NAV_MENU_RESOURCES: document.getElementById('megamenu-resources')[prop],
-                };
-                resolve(data);
-            } else {
-                reject(new Error(error));
-            }
+        const data = {
+          NAV_SCRIPTS: document.getElementById("scripts")[prop],
+          NAV_STYLES: document.getElementById("styles")[prop],
+          NAV_SKIPLINKS: document.getElementById("skiplinks")[prop],
+          NAV_HEADING: document.getElementById("header-withmenu")[prop],
+          NAV_FOOTER: document.getElementById("footer-withmenu")[prop],
+          NAV_MENU_RESOURCES: document.getElementById("megamenu-resources")[
+            prop
+          ]
         };
+        resolve(data);
+      } else {
+        reject(new Error(error));
+      }
+    };
 
-        requestDecorator(callback);
-    });
+    requestDecorator(callback);
+  });
 
 module.exports = getDecorator;
