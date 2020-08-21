9dd3cce281b812b094e535ec2a0751c8278f7969
Sindre Dahl Løken <sindre.loken@nav.no>
2020-08-21 13:05:31 +0200

    lagt til isAlive og isReady paths

--- src/server/routes/internals.js
+++ src/server/routes/internals.js
@@ -1,5 +1,5 @@
-const paths = require('../../paths');
+const paths = require("../../paths");
 module.exports = app => {
-    app.get(paths.isAlivePath, (req, res) => res.sendStatus(200));
-    app.get(paths.isReadyPath, (req, res) => res.sendStatus(200));
+  app.get(paths.isAlivePath, (req, res) => res.sendStatus(200));
+  app.get(paths.isReadyPath, (req, res) => res.sendStatus(200));
 };
