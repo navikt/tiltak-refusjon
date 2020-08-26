468a5bc07971f8810b3fcbe6d8628599242ae540
Mats Bolstad <mbolstad@gmail.com>
2020-08-26 13:01:00 +0200

    Sletter innloggingslinje

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Forside from "./Forside/Forside";
-import Innloggingslinje from "./InnloggingBoundary/Innloggingslinje";
 import { basePath } from "./paths.json";
 import RefusjonSide from "./RefusjonSide/RefusjonSide";
-
-const innloggetSom = {
-    identifikator: "t115435",
-    erNavAnsatt: true,
-    organisasjoner: [
-        {
-            bedriftNavn: "NAV",
-            bedriftNr: "12345"
-        }
-    ]
-};
 
 function App() {
     return (
         <Router basename={basePath}>
-            <Innloggingslinje innloggetBruker={innloggetSom}/>
             <Route exact path="/">
                 <Forside/>
             </Route>
