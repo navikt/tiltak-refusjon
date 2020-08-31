2fe2d61754328b7c9a78922e985856855cbcff95
Mats Bolstad <mbolstad@gmail.com>
2020-08-31 13:41:35 +0200

    Sletter dekoratør, skal lastes inn fra tiltak-refusjon-innside og tiltak-refusjon-utside

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Forside from "./Forside/Forside";
-import Innloggingslinje from "./InnloggingBoundary/Innloggingslinje";
 import { basePath } from "./paths.json";
 import RefusjonSide from "./RefusjonSide/RefusjonSide";
-import { InternDekorator } from "./InternDekorator/InternDekorator";
-
-const innloggetSom = {
-  identifikator: "t115435",
-  erNavAnsatt: true,
-  organisasjoner: [
-    {
-      bedriftNavn: "NAV",
-      bedriftNr: "12345",
-    },
-  ],
-};
 
 function App() {
   return (
     <Router basename={basePath}>
-      <InternDekorator />
-      <Innloggingslinje innloggetBruker={innloggetSom} />
       <Route exact path="/">
         <Forside />
       </Route>
