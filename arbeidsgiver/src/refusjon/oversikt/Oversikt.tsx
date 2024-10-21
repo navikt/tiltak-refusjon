d70b15c8170fbba241286e46b5e191b368930c06
d142796 <millad.dagdoni@nav.no>
2024-10-21 13:45:19 +0200

    lagt til visning av linkPanel for Oversikt.tsx slik at det kun vises når bruker besøker oss via mobil/liten skjerm under 700px...
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- arbeidsgiver/src/refusjon/oversikt/Oversikt.tsx
+++ arbeidsgiver/src/refusjon/oversikt/Oversikt.tsx
@@ -11,9 +11,12 @@ import ArbeidsgiverTableBody from '~/OversiktTabell/TableBody/ArbeidsgiverTableB
 import ArbeidsgiverTableHeader from '~/OversiktTabell/TableHeader/ArbeidsgiverTableHeader';
 
 
-import { LinkPanel, Pagination } from '@navikt/ds-react';
+import { LinkPanel, Pagination , BodyShort} from '@navikt/ds-react';
 import './Oversikt.less';
 import BEMHelper from '~/utils/bem';
+import { formatterDato, formatterPeriode, NORSK_DATO_FORMAT_SHORT } from '~/utils';
+import StatusTekst from '~/StatusTekst/StatusTekst';
+import { useNavigate } from 'react-router-dom';
 const cls = BEMHelper('oversikt');
 
 const Oversikt: FunctionComponent = () => {
@@ -22,6 +25,7 @@ const Oversikt: FunctionComponent = () => {
     const { filter, oppdaterFilter } = useFilter();
     const pageable = useHentRefusjoner(brukerContext, filter);
     const { refusjoner } = pageable;
+    const navigate = useNavigate();
     useOppdaterPagedata(pageable, valgtBedrift, setValgtBedrift);
     antallRefusjoner(refusjoner.length > 0 ? refusjoner.length : 0);
 
@@ -29,8 +33,61 @@ const Oversikt: FunctionComponent = () => {
         <nav className={cls.className} aria-label="Main">
                 {refusjoner.length > 0 ? (
                     <>
-                        <LinkPanel className={cls.element('linkPanel')} refusjoner={pageable.refusjoner}/>
-                  
+                            {pageable.refusjoner.map((refusjon) => (
+                                <LinkPanel
+                                    className={cls.element('linkPanel')}
+                                    border={false}
+                                    role="listitem"
+                                    key={refusjon.id}
+                                    onClick={(event) => {
+                                        event.preventDefault();
+                                        navigate({
+                                            pathname: `/refusjon/${refusjon.id}`,
+                                            search: window.location.search,
+                                        });
+                                    }}
+                                    href={`/refusjon/${refusjon.id}`}
+                                >
+                                    <LinkPanel.Title className={cls.element('linkpanel_title_row')}>
+                                        <BodyShort
+                                            size="small"
+                                            className={cls.element('title_row_column')}
+                                            aria-labelledby={cls.element('deltaker')}
+                                        >
+                                            {refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.deltakerFornavn}{' '}
+                                            {refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.deltakerEtternavn}
+                                        </BodyShort>
+                                        <BodyShort
+                                            size="small"
+                                            className={cls.element('title_row_column')}
+                                            aria-labelledby={cls.element('periode')}
+                                        >
+                                            {formatterPeriode(
+                                                refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddFom,
+                                                refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddTom,
+                                                NORSK_DATO_FORMAT_SHORT
+                                            )}
+                                        </BodyShort>
+                                        <div className={cls.element('title_row_column')}>
+                                            <StatusTekst
+                                                tiltakstype={refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tiltakstype}
+                                                status={refusjon.status}
+                                                tilskuddFom={refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddFom}
+                                                tilskuddTom={refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddTom}
+                                                fratrekkRefunderbarBeløp={refusjon.refusjonsgrunnlag.fratrekkRefunderbarBeløp}
+                                            />
+                                        </div>
+                                        <BodyShort
+                                            size="small"
+                                            className={cls.element('title_row_column')}
+                                            aria-labelledby={cls.element('frist-godkjenning')}
+                                        >
+                                            {formatterDato(refusjon.fristForGodkjenning)}
+                                        </BodyShort>
+                                    </LinkPanel.Title>
+                                </LinkPanel>
+                            ))}
+
                         <OversiktTabell tableHeader={<ArbeidsgiverTableHeader filter={filter} oppdaterFilter={oppdaterFilter} />} tableBody={<ArbeidsgiverTableBody refusjoner={pageable.refusjoner}/>} />
                        
                         <div style={{ display: 'flex', justifyContent: 'center', margin: '1rem' }}>
