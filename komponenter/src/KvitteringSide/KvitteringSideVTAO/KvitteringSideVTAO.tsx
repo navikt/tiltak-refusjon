e37f87d8d6e4593c920622717cbb7f172d87594d
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-12-19 16:35:21 +0100

    Fiks tekster på kvitteringsside
    * Hvis VTAO, vis at refusjon vil sendes inn automatisk
    * Hvis utbetalt, vis datoen refusjonen ble sendt
    * Utbetalt-statusetikett inneholder ikke sendt-dato
    * "For tidlig" = "Sendes ..." for VTAO
    * Fjern VTAO-beløp i informasjonstekst
    * For tidlig-tekst i Statusmelding (treffer VTAO)

--- komponenter/src/KvitteringSide/KvitteringSideVTAO/KvitteringSideVTAO.tsx
+++ komponenter/src/KvitteringSide/KvitteringSideVTAO/KvitteringSideVTAO.tsx
@@ -1,10 +1,10 @@
-import { BodyLong, ErrorMessage, Heading, Tag } from '@navikt/ds-react';
-import { FunctionComponent, ReactElement, useState } from 'react';
+import { BodyLong, Heading, Tag } from '@navikt/ds-react';
+import { FunctionComponent, ReactElement } from 'react';
 import Boks from '~/Boks/Boks';
 import { statusTekst } from '~/types/messages';
 import { Korreksjonsgrunn, Refusjon } from '~/types/refusjon';
 import { RefusjonStatus } from '~/types/status';
-import { formatterDato, NORSK_DATO_FORMAT, NORSK_DATO_OG_TID_FORMAT } from '~/utils';
+import { formatterDato, NORSK_DATO_FORMAT } from '~/utils';
 import { storForbokstav } from '~/utils/stringUtils';
 import VerticalSpacer from '~/VerticalSpacer';
 import InformasjonFraAvtalenVTAO from './InformasjonFraAvtaleVTAO';
@@ -14,26 +14,27 @@ import { InnloggetBruker } from '~/types/brukerContextType';
 import OpprettKorreksjon from '~/knapp/OpprettKorreksjon';
 import Statusmelding from '~/KvitteringSide/Statusmelding';
 import LagreSomPdfKnapp from '~/KvitteringSide/LagreSomPdfKnapp';
+import moment from 'moment';
 
 export const etikettForRefusjonStatus = (refusjon: Refusjon): ReactElement => {
+    const statusetikettTekst =
+        refusjon.status == RefusjonStatus.FOR_TIDLIG
+            ? `sendes ${formatterDato(moment(refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddTom).add(1, 'days').toString())}`
+            : statusTekst[refusjon.status];
     if (refusjon.status === RefusjonStatus.UTBETALING_FEILET) {
-        return <Tag variant="error">{storForbokstav(statusTekst[refusjon.status])} </Tag>;
+        return <Tag variant="error">{storForbokstav(statusetikettTekst)} </Tag>;
     } else if (refusjon.status === RefusjonStatus.UTBETALT) {
         return (
             <Tag variant="info">
-                {storForbokstav(statusTekst[RefusjonStatus.SENDT_KRAV])}{' '}
-                {refusjon.godkjentAvArbeidsgiver && formatterDato(refusjon.godkjentAvArbeidsgiver, NORSK_DATO_FORMAT)}
-                {', '}
-                {storForbokstav(statusTekst[refusjon.status])}{' '}
+                {storForbokstav(statusetikettTekst)}{' '}
                 {refusjon.utbetaltTidspunkt && formatterDato(refusjon.utbetaltTidspunkt, NORSK_DATO_FORMAT)}
             </Tag>
         );
     } else {
         return (
             <Tag variant="info">
-                {storForbokstav(statusTekst[refusjon.status])}{' '}
-                {refusjon.godkjentAvArbeidsgiver &&
-                    formatterDato(refusjon.godkjentAvArbeidsgiver, NORSK_DATO_OG_TID_FORMAT)}
+                {storForbokstav(statusetikettTekst)}{' '}
+                {refusjon.godkjentAvArbeidsgiver && formatterDato(refusjon.godkjentAvArbeidsgiver, NORSK_DATO_FORMAT)}
             </Tag>
         );
     }
@@ -49,48 +50,44 @@ interface Props {
     ) => Promise<void>;
 }
 const KvitteringSideVTAO: FunctionComponent<Props> = ({ refusjon, innloggetBruker, opprettKorreksjon }) => {
-    const [feilmelding, setFeilmelding] = useState<string>('');
     return (
         <>
-            {feilmelding && <ErrorMessage>{feilmelding}</ErrorMessage>}
             <Boks variant="hvit">
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    {innloggetBruker !== undefined && (
-                        <>
-                            {innloggetBruker.harKorreksjonTilgang &&
-                                refusjon.status !== RefusjonStatus.UTBETALING_FEILET &&
-                                !refusjon.korreksjonId &&
-                                opprettKorreksjon !== undefined && (
-                                    <OpprettKorreksjon
-                                        tiltakType={refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tiltakstype}
-                                        opprettKorreksjon={opprettKorreksjon}
-                                    />
-                                )}
-                            <div style={{ marginBottom: '10px', float: 'right' }}>
-                                {etikettForRefusjonStatus(refusjon)}
-                            </div>
-                        </>
-                    )}
-                </div>
-                <div style={{ display: 'flex',  justifyContent: 'space-between', flexDirection: 'row-reverse' }}>
-                    <div style={{ marginLeft: '5rem' }}>
-                        <LagreSomPdfKnapp avtaleId={refusjon.id} />
-                    </div>
-                    {etikettForRefusjonStatus(refusjon)}
+                    {innloggetBruker !== undefined &&
+                        innloggetBruker.harKorreksjonTilgang &&
+                        refusjon.status !== RefusjonStatus.UTBETALING_FEILET &&
+                        !refusjon.korreksjonId &&
+                        opprettKorreksjon !== undefined && (
+                            <OpprettKorreksjon
+                                tiltakType={refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tiltakstype}
+                                opprettKorreksjon={opprettKorreksjon}
+                            />
+                        )}
                 </div>
                 <VerticalSpacer rem={1} />
                 <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Heading level="2" size="large">
                         Refusjon for Varig tilrettelagt arbeid i ordinær virksomhet (VTA-O)
                     </Heading>
+                    {etikettForRefusjonStatus(refusjon)}
                 </div>
                 <VerticalSpacer rem={1} />
-                <Statusmelding status={refusjon.status} />
-                <VerticalSpacer rem={1} />
+                <div style={{ display: 'flex', justifyContent: 'space-between', gap: '5rem' }}>
+                    <Statusmelding
+                        status={refusjon.status}
+                        sendesDato={formatterDato(
+                            moment(refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddTom).add(1, 'days').toString()
+                        )}
+                        vtao={true}
+                        sendtTidspunkt={refusjon.godkjentAvArbeidsgiver}
+                    />
+                    <LagreSomPdfKnapp avtaleId={refusjon.id} />
+                </div>
+                <VerticalSpacer rem={2} />
                 <BodyLong>
-                    Arbeidsgiveren får et tilskudd fra NAV for varig tilrettelagt arbeid. Tilskuddssatsen er 6 808
-                    kroner per måned. Satsen settes årlig av departementet og avtale- og refusjonsløsningen vil
-                    automatisk oppdateres når det kommer nye satser.
+                    Arbeidsgiveren får et tilskudd fra NAV for varig tilrettelagt arbeid. Satsen settes årlig av
+                    departementet og avtale- og refusjonsløsningen vil automatisk oppdateres når det kommer nye satser.
                 </BodyLong>
                 <VerticalSpacer rem={1} />
                 <InformasjonFraAvtalenVTAO
