21e6925a8cacd3ab2415df4aac97e0bd3b2dc8fb
Eirik Vigeland <eirik.vigeland@nav.no>
2025-04-22 11:40:37 +0200

    feat. adressesperre pop-up for arbeidsgiver (#35)
    * Ved åpning av en refusjon hvor deltaker har adressesperre

---
+++ arbeidsgiver/src/komponenter/refusjon-kontroll/RefusjonKontroll.tsx
@@ -0,0 +1,82 @@
+import React, { FunctionComponent, PropsWithChildren, useRef, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { BodyLong, Button, Loader, Modal } from '@navikt/ds-react';
+import { Locked } from '@navikt/ds-icons';
+
+import { useAvtaleKreverAktsomhet } from '@/services/rest-service';
+
+import styles from './RefusjonKontroll.module.less';
+
+const RefusjonKontroll: FunctionComponent<PropsWithChildren> = (props) => {
+    const { refusjonId } = useParams<{ refusjonId: string }>();
+    const { isLoading, data } = useAvtaleKreverAktsomhet(refusjonId);
+
+    const navigate = useNavigate();
+    const ref = useRef<HTMLDialogElement>(null);
+    const [isGodkjent, setGodkjent] = useState<boolean>(false);
+
+    if (isLoading) {
+        return (
+            <div className={styles.container}>
+                <Loader variant="neutral" size="xlarge" />
+            </div>
+        );
+    }
+
+    if (!data?.kreverAktsomhet || isGodkjent) {
+        return props.children;
+    }
+
+    return (
+        <>
+            <Modal
+                ref={ref}
+                open={true}
+                header={{
+                    icon: <Locked title="Lås" />,
+                    heading: 'Deltaker har adressebeskyttelse',
+                }}
+                onClose={() => {
+                    if (window.history.state.idx > 1) {
+                        navigate(-1);
+                    } else {
+                        navigate('/refusjon');
+                    }
+                }}
+            >
+                <Modal.Body>
+                    <BodyLong>
+                        Denne personen har hemmelig adresse og du må derfor utvise aktsomhet. Du skal kun gjøre oppslag
+                        dersom det er nødvendig for å levere tjenesten til denne personen. Nav logger ditt navn og
+                        tidspunkt for oppslaget.
+                    </BodyLong>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        type="button"
+                        onClick={() => {
+                            setGodkjent(true);
+                        }}
+                    >
+                        Vis refusjon
+                    </Button>
+                    <Button
+                        type="button"
+                        variant="secondary"
+                        onClick={() => {
+                            if (window.history.state.idx > 1) {
+                                navigate(-1);
+                            } else {
+                                navigate('/refusjon');
+                            }
+                        }}
+                    >
+                        Avslutt
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
+
+export default RefusjonKontroll;
