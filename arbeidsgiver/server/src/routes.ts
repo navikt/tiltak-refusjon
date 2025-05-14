5facbcf1e088e8c9c4c1cf7f2c24170e1baba903
Eirik Vigeland <eirik.vigeland@nav.no>
2025-05-14 13:06:05 +0200

    feat. bruke oasis for token-validering - arbeidsgiver
    * Gå over til å bruke oasis for token-validering
    * Fjernet ubrukte/undøvendige pakker
    * Oppdatert utdaterte pakker

---
+++ arbeidsgiver/server/src/routes.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import path from 'path';
+
+import apiProxy from '@/proxy/api-proxy';
+import decoratorProxy from '@/proxy/decorator-proxy';
+import { isTokenValid } from '@/auth/token';
+
+const router = express.Router();
+
+export default () => {
+    const isLabs = process.env.MILJO === 'dev-gcp-labs';
+    router.get('/isAlive', (_, res) => {
+        res.status(200).send('Alive');
+    });
+    router.get('/isReady', (_, res) => {
+        res.status(200).send('Ready');
+    });
+
+    if (isLabs) {
+        router.use('/logout', (req, res) => {
+            res.clearCookie('tokenx-token');
+            res.redirect('/');
+        });
+    } else {
+        router.use(async (req, res, next) => {
+            if (req.headers.authorization && (await isTokenValid(req))) {
+                next();
+            } else {
+                res.redirect('/login');
+            }
+        });
+
+        apiProxy(router);
+    }
+
+    decoratorProxy(router, isLabs);
+
+    router.use(express.static(path.join(__dirname, '../build')));
+
+    router.get('/{*splat}', (_, res) => {
+        res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    });
+
+    return router;
+};
