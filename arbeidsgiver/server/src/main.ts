6831a418d08c073bd4d0c5aaa8d83412088098e1
Eirik Vigeland <eirik.vigeland@nav.no>
2025-05-15 08:27:59 +0200

    fix. outbound rules for nav-dekoratoren

--- arbeidsgiver/server/src/main.ts
+++ arbeidsgiver/server/src/main.ts
@@ -1,16 +1,30 @@
-import express from 'express';
+import express, { Handler } from 'express';
 import compression from 'compression';
 import helmet from 'helmet';
+import { buildCspHeader } from '@navikt/nav-dekoratoren-moduler/ssr';
 
 import routes from '@/routes';
 
 const server = express();
+
+const eksternCspMiddleware = (): Handler => {
+    let csp: string;
+
+    return async (_, res, next) => {
+        if (!csp) {
+            csp = await buildCspHeader({}, { env: process.env.MILJO === 'prod-gcp' ? 'prod' : 'dev' });
+        }
+        res.setHeader('Content-Security-Policy', csp);
+        next();
+    };
+};
 
 function start() {
     try {
         server.use(compression());
         server.disable('x-powered-by');
         server.use(helmet({ contentSecurityPolicy: false }));
+        server.use(eksternCspMiddleware());
 
         server.use('/', routes());
 
