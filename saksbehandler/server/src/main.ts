202460198ebcdba41e801dd734f11c84d48934f3
Eirik Vigeland <eirik.vigeland@nav.no>
2025-05-21 11:59:12 +0200

    test

--- saksbehandler/server/src/main.ts
+++ saksbehandler/server/src/main.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
 
 import routes from '@/routes';
 
@@ -6,7 +8,23 @@ const server = express();
 
 function startApp() {
     try {
+        server.use(compression());
         server.disable('x-powered-by');
+
+        server.use(
+            helmet({
+                contentSecurityPolicy: {
+                    useDefaults: true,
+                    directives: {
+                        'default-src': ["'self'", 'wss://*.nav.no', '*.nav.no'],
+                        'script-src': ["'self'", '*.nav.no', '*.adeo.no', "'unsafe-inline'"],
+                        'style-src': ["'self'", '*.nav.no', '*.adeo.no', "'unsafe-inline'"],
+                        'font-src': ["'self'", '*.nav.no', 'data:'],
+                        'img-src': ["'self'", '*.nav.no'],
+                    },
+                },
+            })
+        );
 
         // setup routes
         server.use('/', routes());
