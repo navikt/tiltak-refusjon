c12cf7a2311290b22255bfb09be4da60dd36ff91
Magnus Rom <magnus.skalleberg.rom@nav.no>
2025-07-02 15:57:13 +0200

    gjordt om any til unknown

--- komponenter/src/utils/predicates.ts
+++ komponenter/src/utils/predicates.ts
@@ -1,4 +1,4 @@
 /**
  * Sjekker om en verdi er null eller undefined
  */
-export const erNil = (x: any): x is null | undefined => x === null || x === undefined;
+export const erNil = (x: unknown): x is null | undefined => x === null || x === undefined;
