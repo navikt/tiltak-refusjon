6319e48443fc7fafa401f5b6f01ed4e4f0ee7ecc
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2025-10-27 14:09:33 +0100

    Oppgrader storybook

--- arbeidsgiver/src/stories/RefusjonMedMinusbelop.stories.tsx
+++ arbeidsgiver/src/stories/RefusjonMedMinusbelop.stories.tsx
@@ -1,6 +1,6 @@
 import KvitteringSide from '@/refusjon/KvitteringSide/KvitteringSide';
 
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Diskresjonskode } from '~/types';
 import { Refusjon } from '~/types/refusjon';
 import { RefusjonStatus } from '~/types/status';
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 // @ts-check
 
 import eslint from '@eslint/js';
@@ -9,4 +12,4 @@ export default tseslint.config({
         eslint.configs.recommended,
         tseslint.configs.recommended,
     ]
-});
+}, storybook.configs["flat/recommended"], storybook.configs["flat/recommended"]);
