b0f6a46c534e94c2393949add4008436acfb7e0b
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-05-29 15:44:40 +0200

    Tar i bruk webcomponent versjon av interndekorator
    Arbeidsgiver-visningen hadde en CSS-stil her som refererte
    til en farge som ikke eksisterte, og dermed ble operatorene
    usynlige.

--- saksbehandler/src/index.tsx
+++ saksbehandler/src/index.tsx
@@ -4,6 +4,11 @@ import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+const script = document.createElement('script');
+script.type = 'module';
+script.src = '/internarbeidsflatedecorator/internarbeidsflate-decorator.wc.js';
+document.head.appendChild(script);
+
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container!);
 root.render(
