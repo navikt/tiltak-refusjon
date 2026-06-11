a7df486ca098ecdcbb86397b27795237323c6da3
Philip Puente <Philip.Oygarden.Puente@nav.no>
2026-06-11 12:26:31 +0200

    Tar i bruk webcomponent versjon av interndekorator (#89)
    Arbeidsgiver-visningen hadde en CSS-stil her som refererte
    til en farge som ikke eksisterte, og dermed ble operatorene
    usynlige.
    
    Co-authored-by: Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>

--- saksbehandler/src/vite-env.d.ts
+++ saksbehandler/src/vite-env.d.ts
@@ -1,2 +1,22 @@
 /// <reference types="vite-plugin-svgr/client" />
 /// <reference types="vite/client" />
+
+declare namespace React {
+    namespace JSX {
+        interface IntrinsicElements {
+            'internarbeidsflate-decorator': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+                'app-name'?: string;
+                environment?: string;
+                proxy?: string;
+                'fetch-active-enhet-on-mount'?: boolean;
+                'fetch-active-user-on-mount'?: boolean;
+                'fnr-sync-mode'?: string;
+                'enhet-sync-mode'?: string;
+                fnr?: string;
+                'url-format'?: string;
+                'enable-hotkeys'?: boolean;
+                'show-hotkeys'?: boolean;
+            };
+        }
+    }
+}
